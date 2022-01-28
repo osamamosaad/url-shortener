683511d43286265a26b5111377c6c6fe2a1925db
Osama Mosaad <osama.mosaad@basharsoft.com>
2022-01-28 02:08:00 +0200

    feat: implement the logic of encode

--- handlers/encode.go
+++ handlers/encode.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
+	generator "github.com/osamamosaad/url-shortener-api-ugnxea/pkg/Generator"
+	"github.com/osamamosaad/url-shortener-api-ugnxea/pkg/counter"
 	"github.com/osamamosaad/url-shortener-api-ugnxea/pkg/models"
 )
 
@@ -12,5 +14,27 @@ type Encode struct {
 }
 
 func (e *Encode) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Path)
+	bodyIo := json.NewDecoder(r.Body)
+	var request request
+	if err := bodyIo.Decode(&request); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "application/json")
+		jsonResp, _ := json.Marshal(errorResponse{"badRequest"})
+		w.Write(jsonResp)
+	}
+
+	id := counter.Get()
+	shortCode := generator.Create(id)
+	url := models.Url{
+		ID:      id,
+		Short:   shortCode,
+		Orignal: request.Url,
+	}
+
+	res, _ := e.Db.Insert(url)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	jsonResp, _ := json.Marshal(res)
+	w.Write(jsonResp)
 }
