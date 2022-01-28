c875221df26a15843e63e8ccee7334ed8602c94a
Osama Mosaad <osama.mosaad@basharsoft.com>
2022-01-28 02:20:24 +0200

    refactor: refactor encode handler

--- handlers/encode.go
+++ handlers/encode.go
@@ -14,27 +14,37 @@ type Encode struct {
 }
 
 func (e *Encode) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	bodyIo := json.NewDecoder(r.Body)
 	var request request
+	bodyIo := json.NewDecoder(r.Body)
 	if err := bodyIo.Decode(&request); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Header().Set("Content-Type", "application/json")
-		jsonResp, _ := json.Marshal(errorResponse{"badRequest"})
-		w.Write(jsonResp)
+		responseWriter(http.StatusBadRequest, errorResponse{"Bad request!"}, w)
+		return
 	}
 
+	url, err := e.insert(request)
+
+	if err != nil {
+		responseWriter(
+			http.StatusInternalServerError,
+			errorResponse{"Something went wrong!, please try after while."},
+			w,
+		)
+		return
+	}
+	responseWriter(http.StatusOK, url, w)
+}
+
+func (e *Encode) insert(request request) (models.Url, error) {
+	// Get the next number from counter service
 	id := counter.Get()
+	// Generate short key
 	shortCode := generator.Create(id)
+
+	// Build model
 	url := models.Url{
 		ID:      id,
 		Short:   shortCode,
 		Orignal: request.Url,
 	}
-
-	res, _ := e.Db.Insert(url)
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	jsonResp, _ := json.Marshal(res)
-	w.Write(jsonResp)
+	return e.Db.Insert(url)
 }
